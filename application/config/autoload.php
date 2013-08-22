79bada4fe5a65dfd8149e848e7ed1a342a295f60
cherra <jorge@matica.mx>
2013-08-21 21:35:18 -0500

    Autoload helper Url

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -64,7 +64,7 @@ $autoload['libraries'] = array('database', 'session');
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('path');
+$autoload['helper'] = array('path', 'url');
 
 
 /*
