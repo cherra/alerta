a2bb5e893a256dc155c231d44173e80a2d050dd5
cherra <jorge@matica.mx>
2013-09-12 14:17:48 -0500

    Nuevos metodos en el WS para la App: get_categorias() y get_incidentes.

--- application/controllers/ws/app.php
+++ application/controllers/ws/app.php
@@ -14,12 +14,53 @@ class App extends CI_Controller {
         parent::__construct();
     }
     
+    public function get_categorias(){
+        if( ($datos = $this->input->get()) ){
+            $this->load->model('categoria','c');
+            $categorias = $this->c->get_all();
+            if($categorias->num_rows() > 0){
+                $resultado = $categorias->result_array();
+                if(isset($datos['callback'])){
+                    $this->output->set_content_type('text/javascript')->set_output($datos['callback'].'('.json_encode($resultado, JSON_FORCE_OBJECT).')');
+                }else{
+                    echo json_encode($resultado, JSON_FORCE_OBJECT);
+                }
+            }else{
+                return json_encode(false);
+            }
+        }
+    }
+    
+    public function get_incidentes(){
+        if( ($datos = $this->input->get()) ){
+            $this->load->model('incidente','i');
+            $incidentes = $this->i->get_menu_by_categoria($datos['id_categoria']);
+            if($incidentes->num_rows() > 0){
+                $resultado = $incidentes->result_array();
+                if(isset($datos['callback'])){
+                    $this->output->set_content_type('text/javascript')->set_output($datos['callback'].'('.json_encode($resultado, JSON_FORCE_OBJECT).')');
+                }else{
+                    echo json_encode($resultado, JSON_FORCE_OBJECT);
+                }
+            }else{
+                if(isset($datos['callback'])){
+                    $this->output->set_content_type('text/javascript')->set_output($datos['callback'].'('.json_encode(false).')');
+                }else{
+                    return json_encode(false);
+                }
+            }
+        }else{
+            echo json_encode(false);
+        }
+    }
+    
     public function insert_reporte(){
         if( ($reporte = $this->input->get()) ){
             $this->load->model('reporte','r');
             $this->load->library('uuid');
             $datos['id'] = $this->uuid->v4();
             $datos['id_persona'] = $reporte['id_persona'];
+            $datos['id_tipo'] = $reporte['id_tipo'];
             $datos['mensaje'] = $reporte['mensaje'];
             //$datos['id_persona'] = $reporte['id_persona'];
             if( $this->r->save($datos) ){
