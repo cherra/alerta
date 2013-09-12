a6fec649e70b56e2fd14b5fc590d00e8fe63d483
cherra <jorge@matica.mx>
2013-09-12 10:45:46 -0500

    Modelos Persona y Reporte (inicial).

---
+++ application/models/persona.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Description of persona
+ *
+ * @author cherra
+ */
+class Persona extends CI_Model {
+    
+    private $tbl = 'Persona';
+    
+    /*
+     * Cuenta todos los registros utilizando un filtro de busqueda
+     */
+    function count_all( $filtro = NULL ) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('id',$f);
+            }
+        }
+        $query = $this->db->get($this->tbl);
+        return $query->num_rows();
+    }
+    
+    /**
+     *  Obtiene todos los registros de la tabla
+     */
+    function get_all() {
+        $this->db->order_by('id','asc');
+        return $this->db->get($this->tbl);
+    }
+    
+    /**
+    * Cantidad de registros por pagina
+    */
+    function get_paged_list($limit = NULL, $offset = 0, $filtro = NULL) {
+        if(!empty($filtro)){
+            $filtro = explode(' ', $filtro);
+            foreach($filtro as $f){
+                $this->db->or_like('id',$f);
+            }
+        }
+        $this->db->order_by('id','asc');
+        return $this->db->get($this->tbl, $limit, $offset);
+    }
+    
+    /**
+    * Obtener por id
+    */
+    function get_by_id($id) {
+        $this->db->where('id', $id);
+        return $this->db->get($this->tbl);
+    }
+    
+    function valida($email, $password){
+        if(!empty($email) && !empty($password)){
+            $this->db->where('email', $email);
+            $this->db->where('password', $password);
+            return $this->db->get($this->tbl);
+        }
+    }
+    
+    /**
+    * Alta
+    */
+    function save( $datos ) {
+        $this->db->insert($this->tbl, $datos);
+        return $this->db->insert_id();
+    }
+
+    /**
+    * Actualizar por id
+    */
+    function update($id, $datos) {
+        $this->db->where('id', $id);
+        $this->db->update($this->tbl, $datos);
+    }
+
+    /**
+    * Eliminar por id
+    */
+    function delete($id) {
+        $this->db->where('id', $id);
+        $this->db->delete($this->tbl);
+    } 
+}
+?>
